e5e0796456012addb94ba52dc7616faf57e07eba
kmaroff <artem.komar@gmail.com>
2017-08-31 01:51:43 +0400

    Добавил вызов окна с оповещением после отправки

--- script/script.js
+++ script/script.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
 
 	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+	$("#contact").submit(function() { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function() {
+	//Вывод окна с оповещением классом visible после отправки
+      $(".success").addClass("visible");
+      setTimeout(function() {
+        // Done Functions
+        th.trigger("reset");
+	//сброс формы и закрытие окна
+        $(".success").removeClass("visible");
+        $.fancybox.close();
+      }, 3500);
+    });
+    return false;
+  });
 
 });
